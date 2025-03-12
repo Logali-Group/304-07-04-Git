eb2dd78076a73e4c73a947731c80d3c49b2755c6
Jorge Luis Briceño Amesty <jbriceno@logaligroup.com>
2025-03-12 13:58:38 +0000

    Add Filter

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,4 +1,9 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import { SearchField$SearchEvent } from "sap/m/SearchField";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import Table from "sap/m/Table";
+import ListBinding from "sap/ui/model/ListBinding";
 
 /**
  * @namespace git.controller
@@ -7,6 +12,27 @@ export default class Main extends Controller {
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
+
+    }
+
+    public onFilterSearch (event : SearchField$SearchEvent) : void {
+
+        let sValue = event.getParameter("query");
+        let aFilters = [];
+
+        if (sValue) {
+            aFilters.push(new Filter({
+                filters: [
+                    new Filter("ID", FilterOperator.EQ, sValue),
+                    new Filter("FullName", FilterOperator.Contains, sValue)
+                ],
+                and: false
+            }))
+        }
+
+        let oTable= this.byId("table") as Table;
+        let binding = oTable.getBinding("items") as ListBinding;
+        binding.filter(aFilters);
 
     }
 }
